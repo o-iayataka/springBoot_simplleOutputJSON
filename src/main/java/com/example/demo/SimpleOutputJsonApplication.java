b737a4ce67f07eb08e3dc25b80b242c726c89042
ayataka <65064787+o-iayataka@users.noreply.github.com>
2022-04-05 23:36:01 +0900

    Add files via upload

---
+++ src/main/java/com/example/demo/SimpleOutputJsonApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleOutputJsonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleOutputJsonApplication.class, args);
+	}
+
+}
